e96044b6780df9edf38ab3287cd723af81a931c2
goujou <goujou@gmx.de>
2023-07-06 14:34:02 +0200

    updatef pdf with new fintsize of Emanuel figure

--- code/simple_entropy.py
+++ code/simple_entropy.py
@@ -8,6 +8,10 @@
 #       format_name: light
 #       format_version: '1.5'
 #       jupytext_version: 1.14.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
 # ---
 
 """Simple entropy examples plots."""
